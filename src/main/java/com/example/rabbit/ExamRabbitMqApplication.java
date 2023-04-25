ccc4a1be0710e9c04cb9479519847eb6bebceb0e
beetrbgus <devbeetrb0214@gmail.com>
2023-04-25 23:40:36 +0900

    Initial commit

---
+++ src/main/java/com/example/rabbit/ExamRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamRabbitMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamRabbitMqApplication.class, args);
+    }
+
+}
